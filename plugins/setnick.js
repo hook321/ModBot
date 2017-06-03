39a99359cd6c7e7f4a2fed908848f6259ec53761
Michael Cao <mike.x.cao@gmail.com>
2017-06-03 19:09:49 -0400

    Setnick

--- plugins/setnick.js
+++ plugins/setnick.js
@@ -5,7 +5,7 @@ module.exports = {
     help: 'Sets a user\'s nickname.',
 	main: function(bot, msg) {
 		const isCommander = ["171319044715053057", "180094452860321793"];
-		if (msg.member.hasPermission('MANAGE_MEMBERS') || msg.member.hasPermission('ADMINISTRATOR') || isCommander.indexOf(msg.author.id) > -1) {
+		if (msg.member.hasPermission('MANAGE_NICKNAMES') || msg.member.hasPermission('ADMINISTRATOR') || isCommander.indexOf(msg.author.id) > -1) {
 			var user = msg.mentions.users.array()[0];
 			var newNick = msg.content.split(" ").splice(1).join(" ").trim();
 			msg.guild.members.get(user).setNickname(newNick)
