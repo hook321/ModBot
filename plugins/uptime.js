5725b817dcc9ae2fb3bb13bb1e4d91bd94dd3292
Michael Cao <mike.x.cao@gmail.com>
2017-06-03 18:52:27 -0400

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -3,13 +3,13 @@ module.exports = {
     usage: '<p>uptime',
     permission: 1,
     help: 'Provides the bot\'s uptime.',
-	main: function(bot, message) {
+	main: function(bot, msg) {
 		var date = new Date(bot.uptime);
 		var strDate = '**';
 		strDate += date.getUTCDate() - 1 + " days, ";
 		strDate += date.getUTCHours() + " hours, ";
 		strDate += date.getUTCMinutes() + " minutes, ";
 		strDate += date.getUTCSeconds() + " seconds**";
-		message.channel.sendMessage("ROBOT UPTIME:\n" + strDate);
+		msg.channel.send(bot.user.username + " Uptime:\n" + strDate);
 	}
 };
